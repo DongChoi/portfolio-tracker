34b7cec4a3e9eaaf75e9a2c17114845417a217c7
atmason90 <atmason90@gmail.com>
2023-05-23 20:34:50 -0700

    finish login form

--- portfolio-tracker/src/utils/mutations.js
+++ portfolio-tracker/src/utils/mutations.js
@@ -1,7 +1,7 @@
 import { gql } from '@apollo/client';
 
 export const LOGIN_USER = gql`
-    mutation login($username: String!, $passwordL String!) {
+    mutation login($username: String!, $password: String!) {
         login(username: $username, password: $password) {
             token
             user {
