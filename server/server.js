559f4f12000195574d91289aa4fae747e8663b85
atmason90 <atmason90@gmail.com>
2023-05-26 15:16:51 -0700

    dbug deployment

--- server/server.js
+++ server/server.js
@@ -19,7 +19,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../portfolio-tracker/build')))
+    // app.use(express.static(path.join(__dirname, '../portfolio-tracker/build')))
+    app.use(express.static('portfolio-tracker/build'))
 }
 
 app.get('/', (req, res) => {
